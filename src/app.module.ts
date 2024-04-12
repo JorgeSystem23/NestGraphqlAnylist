0b8419b9bf8d54c759639ea6e4662dcb7a5e4263
JorgeSystem23 <jgrullon@popoyan.com>
2024-04-12 15:27:53 -0600

    A database is added using docker, to perform real data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
-import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { join } from "path";
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
