00dfa0d6abad761caba34f557fb815f1f9f34e67
JorgeSystem23 <jgrullon@popoyan.com>
2024-04-15 07:29:47 -0600

    A product is searched by its unique identifier (uuid)

--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int } from "@nestjs/graphql";
+import { Resolver, Query, Mutation, Args, Int, ID } from "@nestjs/graphql";
 
 import { ItemsService } from "./items.service";
 import { Item } from "./entities/item.entity";
@@ -17,18 +17,18 @@ export class ItemsResolver {
   }
 
   @Query(() => [Item], { name: "items" })
-  findAll() {
+  async findAll(): Promise<Item[]> {
     return this.itemsService.findAll();
   }
 
   @Query(() => Item, { name: "item" })
-  findOne(@Args("id", { type: () => Int }) id: number) {
+  findOne(@Args("id", { type: () => ID }) id: string) {
     return this.itemsService.findOne(id);
   }
 
   @Mutation(() => Item)
   updateItem(@Args("updateItemInput") updateItemInput: UpdateItemInput) {
-    return this.itemsService.update(updateItemInput.id, updateItemInput);
+    // return this.itemsService.update(updateItemInput);
   }
 
   @Mutation(() => Item)
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { CreateItemInput } from "./dto/inputs/create-item.input";
@@ -18,15 +18,21 @@ export class ItemsService {
     return await this.itemsRepository.save(newItem);
   }
 
-  findAll() {
-    return [];
+  async findAll():Promise<Item[]> {
+    return this.itemsRepository.find(); 
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: string):Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({ id });
+
+    if (!item) {
+      throw new NotFoundException(`Item with id ${id} not found`);
+    }
+
+    return item;
   }
 
-  update(id: number, updateItemInput: UpdateItemInput) {
+  update(id: string, updateItemInput: UpdateItemInput) {
     return `This action updates a #${id} item`;
   }
 
